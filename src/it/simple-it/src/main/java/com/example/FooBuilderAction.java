418457e81e2c4e408191233c98aa99ef13381a22
Jens Bannmann <jens.b@web.de>
2021-10-27 17:12:13 +0200

    Add AG import statement to integration test
    Signed-off-by: Jens Bannmann <jens.b@web.de>

--- src/it/simple-it/src/main/java/com/example/FooBuilderAction.java
+++ src/it/simple-it/src/main/java/com/example/FooBuilderAction.java
@@ -1,7 +1,13 @@
 package com.example;
 
+import java.util.Map;
+
 class FooBuilderAction implements IFooBuilderAction
 {
+    public void configure(Map<String, Object> options)
+    {
+    }
+
     public void parse(String source)
     {
     }
